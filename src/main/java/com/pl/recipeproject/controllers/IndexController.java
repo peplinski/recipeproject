7ce1f9bfd8ccc6642fb1f54cf3d50b17010a9088
Driver <peplinskikrzysztof@gmail.com>
2018-09-02 13:50:20 +0200

    jpa query methods

--- src/main/java/com/pl/recipeproject/controllers/IndexController.java
+++ src/main/java/com/pl/recipeproject/controllers/IndexController.java
@@ -4,10 +4,12 @@ import com.pl.recipeproject.domain.Category;
 import com.pl.recipeproject.domain.UnitOfMeasure;
 import com.pl.recipeproject.repositories.CategoryRepository;
 import com.pl.recipeproject.repositories.UnitOfMeasureRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Controller
 public class IndexController {
 
     private CategoryRepository categoryRepository;
@@ -23,6 +25,9 @@ public class IndexController {
 
         Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
         Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+        System.out.println("Category id is: " + categoryOptional.get().getId());
+        System.out.println("Umo id is: " + unitOfMeasureOptional.get().getId());
 
 
         return "index";
