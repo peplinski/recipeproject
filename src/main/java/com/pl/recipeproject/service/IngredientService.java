051a72853a6815191ed956140983d40c576ce9b3
Driver <peplinskikrzysztof@gmail.com>
2018-09-16 17:02:02 +0200

    show ingredients and service implementation

---
+++ src/main/java/com/pl/recipeproject/service/IngredientService.java
@@ -0,0 +1,7 @@
+package com.pl.recipeproject.service;
+
+import com.pl.recipeproject.commands.IngredientCommand;
+
+public interface IngredientService {
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+}
