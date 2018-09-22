6774cba010787fb0044a11580bd88ef6173dc48b
Driver <peplinskikrzysztof@gmail.com>
2018-09-22 13:52:34 +0200

    image controller, saerive & test

---
+++ src/main/java/com/pl/recipeproject/service/ImageService.java
@@ -0,0 +1,8 @@
+package com.pl.recipeproject.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
