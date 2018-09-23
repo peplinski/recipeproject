82260a2b8fc860816a41f7765835f7c40a496412
Driver <peplinskikrzysztof@gmail.com>
2018-09-23 18:22:30 +0200

    error handler

--- src/main/java/com/pl/recipeproject/controllers/RecipeController.java
+++ src/main/java/com/pl/recipeproject/controllers/RecipeController.java
@@ -1,14 +1,14 @@
 package com.pl.recipeproject.controllers;
 
 import com.pl.recipeproject.commands.RecipeCommand;
+import com.pl.recipeproject.exceptions.NotFoundException;
 import com.pl.recipeproject.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -54,5 +54,18 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handlerNotFound(){
+
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+
+        return modelAndView;
     }
 }
