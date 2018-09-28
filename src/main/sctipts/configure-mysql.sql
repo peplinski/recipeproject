30faf20d5294b04fd34cb1aa708e366530df47d5
Driver <peplinskikrzysztof@gmail.com>
2018-09-28 17:56:34 +0200

    adding circleci config

--- src/main/sctipts/configure-mysql.sql
+++ src/main/sctipts/configure-mysql.sql
@@ -7,10 +7,10 @@ CREATE DATABASE sfg_dev;
 CREATE DATABASE sfg_prod;
 
 #Create database service accounts
-CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'guru';
-CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'guru';
-CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'guru';
-CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'guru';
+CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY 'pepe';
+CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY 'pepe';
+CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY 'pepe';
+CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY 'pepe';
 
 #Database grants
 GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
